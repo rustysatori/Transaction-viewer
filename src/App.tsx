3494f8f768f64a6eb535a5d5e1f4e141650e78a6
Metodi Deribanov <imaginois@gmail.com>
2025-10-16 14:52:06 +0300

    Update title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-    return <h1>Hello, React + TypeScript + Webpack!</h1>;
+    return <h1>Financial transaction browser app</h1>;
 };
 
 export default App;
